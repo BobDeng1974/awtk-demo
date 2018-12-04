220dd22a45645f7857879f5c381741be47b65fcc
zhaoqi <zq@zhaoqi.info>
2018-12-04 18:13:39 +0800

    add:增加首页

--- demo/awtk-demo/assets.c
+++ demo/awtk-demo/assets.c
@@ -8,33 +8,48 @@
 #include "assets/inc/strings/zh_TW.data"
 #include "assets/inc/styles/default.data"
 #include "assets/inc/styles/main.data"
+#include "assets/inc/styles/system_bar.data"
+#include "assets/inc/styles/tab_bottom.data"
 #include "assets/inc/styles/window1.data"
-#include "assets/inc/ui/air_conditioning.data"
-#include "assets/inc/ui/air_quality.data"
 #include "assets/inc/ui/gvs_main.data"
-#include "assets/inc/ui/hvac.data"
 #include "assets/inc/ui/index.data"
-#include "assets/inc/ui/input_number.data"
 #include "assets/inc/ui/loading.data"
-#include "assets/inc/ui/make_color.data"
-#include "assets/inc/ui/mulfuncchildwin.data"
-#include "assets/inc/ui/mulfuncfour.data"
-#include "assets/inc/ui/mulfuncone.data"
-#include "assets/inc/ui/mulfuncthree.data"
-#include "assets/inc/ui/mulfunctwo.data"
-#include "assets/inc/ui/music.data"
-#include "assets/inc/ui/newair_child.data"
-#include "assets/inc/ui/new_air.data"
-#include "assets/inc/ui/radfloor.data"
-#include "assets/inc/ui/realtime_clock.data"
-#include "assets/inc/ui/slide_view_h.data"
-#include "assets/inc/ui/timeset.data"
+#include "assets/inc/ui/qq.data"
+#include "assets/inc/ui/system_bar.data"
 #ifdef WITH_STB_IMAGE
+#include "assets/inc/images/close.res"
+#include "assets/inc/images/contact.res"
+#include "assets/inc/images/contact_active.res"
+#include "assets/inc/images/discovery.res"
+#include "assets/inc/images/discovery_active.res"
 #include "assets/inc/images/g_background.res"
+#include "assets/inc/images/g_menuborder.res"
+#include "assets/inc/images/g_page_number_off.res"
+#include "assets/inc/images/g_page_number_on.res"
 #include "assets/inc/images/logo.res"
+#include "assets/inc/images/me.res"
+#include "assets/inc/images/me_active.res"
+#include "assets/inc/images/msg.res"
+#include "assets/inc/images/msg_active.res"
+#include "assets/inc/images/qq.res"
+#include "assets/inc/images/weichat.res"
 #else
+#include "assets/inc/images/close.data"
+#include "assets/inc/images/contact.data"
+#include "assets/inc/images/contact_active.data"
+#include "assets/inc/images/discovery.data"
+#include "assets/inc/images/discovery_active.data"
 #include "assets/inc/images/g_background.data"
+#include "assets/inc/images/g_menuborder.data"
+#include "assets/inc/images/g_page_number_off.data"
+#include "assets/inc/images/g_page_number_on.data"
 #include "assets/inc/images/logo.data"
+#include "assets/inc/images/me.data"
+#include "assets/inc/images/me_active.data"
+#include "assets/inc/images/msg.data"
+#include "assets/inc/images/msg_active.data"
+#include "assets/inc/images/qq.data"
+#include "assets/inc/images/weichat.data"
 #endif/*WITH_STB_IMAGE*/
 #ifdef WITH_VGCANVAS
 #endif/*WITH_VGCANVAS*/
@@ -55,8 +70,22 @@ ret_t assets_init(void) {
   assets_manager_load(rm, ASSET_TYPE_STYLE, "default");
   assets_manager_load(rm, ASSET_TYPE_FONT, "default");
 #else
+  assets_manager_add(rm, image_close);
+  assets_manager_add(rm, image_contact);
+  assets_manager_add(rm, image_contact_active);
+  assets_manager_add(rm, image_discovery);
+  assets_manager_add(rm, image_discovery_active);
   assets_manager_add(rm, image_g_background);
+  assets_manager_add(rm, image_g_menuborder);
+  assets_manager_add(rm, image_g_page_number_off);
+  assets_manager_add(rm, image_g_page_number_on);
   assets_manager_add(rm, image_logo);
+  assets_manager_add(rm, image_me);
+  assets_manager_add(rm, image_me_active);
+  assets_manager_add(rm, image_msg);
+  assets_manager_add(rm, image_msg_active);
+  assets_manager_add(rm, image_qq);
+  assets_manager_add(rm, image_weichat);
   assets_manager_add(rm, strings_en_US);
   assets_manager_add(rm, strings_ja_JP);
   assets_manager_add(rm, strings_ko_KR);
@@ -64,27 +93,14 @@ ret_t assets_init(void) {
   assets_manager_add(rm, strings_zh_TW);
   assets_manager_add(rm, style_default);
   assets_manager_add(rm, style_main);
+  assets_manager_add(rm, style_system_bar);
+  assets_manager_add(rm, style_tab_bottom);
   assets_manager_add(rm, style_window1);
-  assets_manager_add(rm, ui_air_conditioning);
-  assets_manager_add(rm, ui_air_quality);
   assets_manager_add(rm, ui_gvs_main);
-  assets_manager_add(rm, ui_hvac);
   assets_manager_add(rm, ui_index);
-  assets_manager_add(rm, ui_input_number);
   assets_manager_add(rm, ui_loading);
-  assets_manager_add(rm, ui_make_color);
-  assets_manager_add(rm, ui_mulfuncchildwin);
-  assets_manager_add(rm, ui_mulfuncfour);
-  assets_manager_add(rm, ui_mulfuncone);
-  assets_manager_add(rm, ui_mulfuncthree);
-  assets_manager_add(rm, ui_mulfunctwo);
-  assets_manager_add(rm, ui_music);
-  assets_manager_add(rm, ui_newair_child);
-  assets_manager_add(rm, ui_new_air);
-  assets_manager_add(rm, ui_radfloor);
-  assets_manager_add(rm, ui_realtime_clock);
-  assets_manager_add(rm, ui_slide_view_h);
-  assets_manager_add(rm, ui_timeset);
+  assets_manager_add(rm, ui_qq);
+  assets_manager_add(rm, ui_system_bar);
 #ifdef WITH_VGCANVAS
 #endif/*WITH_VGCANVAS*/
 #endif
--- demo/awtk-demo/awtk_demo_index.c
+++ demo/awtk-demo/awtk_demo_index.c
@@ -39,10 +39,89 @@
 #include "ui_loader/ui_builder_default.h"
 #include "ext_widgets.h"
 
+#if defined(WIN32)
+#include <windows.h>
+#endif
+
 extern ret_t demo_loading_init();
+extern ret_t awtk_demo_qq_init(void* ctx, event_t* e);
+
+static void open_window(const char* name, widget_t* to_close);
+
+static ret_t on_open_window(void* ctx, event_t* e) {
+  window_open((const char*)ctx);
+  (void)e;
+
+  return RET_OK;
+}
+
+
+static ret_t install_one(void* ctx, const void* iter) {
+  widget_t* widget = WIDGET(iter);
+
+  log_debug("%s\r\n", widget->name);
+  if (widget->name != NULL) {
+    const char* name = widget->name;
+    if (strstr(name, "open:qq") != NULL) {
+      log_debug("install %s\r\n", name);
+      widget_on(widget, EVT_CLICK, awtk_demo_qq_init, (void*)(name + 5));
+    }
+  }
+  (void)ctx;
+
+  return RET_OK;
+}
+
+
+static void install_click_hander(widget_t* widget) {
+  widget_foreach(widget, install_one, widget);
+}
+
+
+static void open_window(const char* name, widget_t* to_close) {
+  widget_t* win = to_close ? window_open_and_close(name, to_close) : window_open(name);
+
+  install_click_hander(win);
+
+  if (tk_str_eq(widget_get_type(win), WIDGET_TYPE_DIALOG)) {
+    dialog_modal(win);
+  }
+}
+
+static ret_t awtk_get_currtime(timer_info_t* timer)
+{
+  char text[64];
+  widget_t* win = WIDGET(timer->ctx);
+  widget_t* dis_date = widget_lookup(win, "dis_date", TRUE);
+
+  timer->duration_ms = 1000;
+
+#if defined(WIN32)
+  SYSTEMTIME sys;
+  GetLocalTime(&sys);
+  snprintf(text, sizeof(text), "%02u:%02u:%02u",sys.wHour, sys.wMinute, sys.wSecond);
+#endif
+  widget_set_text_utf8(dis_date, text);
+
+  return RET_REPEAT;
+}
+
+ret_t on_qq_open_window(void* ctx)
+{
+  log_debug("-----------------------------");
+  window_open((const char *)ctx);
+}
 
 ret_t awtk_demo_index() 
 {
+  widget_t *system_bar = window_open("system_bar");
+  widget_t *dis_title = widget_lookup(system_bar, "dis_title", TRUE);
+  widget_set_text_utf8(dis_title, "中国移动");
+
   widget_t *index = window_open("index");
+
+  install_click_hander(index);
+  
+  timer_add(awtk_get_currtime, system_bar, 0);
   return RET_OK;
 }
