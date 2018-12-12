2de2e653b390c5e951c413ebf8013b338e20e0f7
zq <zq@zhaoqi.info>
2018-12-12 20:38:20 +0800

    add:增肌在代码中添加QQ消息

--- demo/awtk-demo/awtk_demo_qq.c
+++ demo/awtk-demo/awtk_demo_qq.c
@@ -19,7 +19,7 @@
  *
  */
 
-#include "awtk.h"
+#include "base/types_def.h"
 #include "base/mem.h"
 #include "base/label.h"
 #include "base/timer.h"
@@ -36,6 +36,7 @@
 #include "base/image_manager.h"
 #include "base/window_manager.h"
 #include "base/widget_factory.h"
+#include "base/view.h"
 #include "ui_loader/ui_builder_default.h"
 #include "ext_widgets.h"
 #include "scroll_view/list_item.h"
@@ -76,30 +77,95 @@ ret_t awtk_demo_qq_init(void* ctx, event_t* e)
   if (qq_view != NULL) {
     log_debug("---------qq_view NOT NULL---------------------------------------\r\n");
 
-    widget_t *new_view1 = view_create(qq_view, 0, 0, 0, 0);
-    widget_use_style(new_view1, "odd_clickable");
-    // widget_layout_t new_view1_layout;
-    // widget_layout_parse(&new_view1_layout, "r:5", "0", "8%", "50");
-    // widget_set_self_layout(new_view1, &new_view1_layout);
-	
-    // widget_t *new_img = image_create(new_view1, 0, 0, 32, 32);
-    // image_set_draw_type(new_img, IMAGE_DRAW_ICON);
-    // image_set_image(new_img, "a");
-	  // widget_t *new_list_item = NULL;
-    // new_list_item = list_item_create(qq_view, 0, 0, 32, 32);
-    // if (new_list_item != NULL) {
-    //   log_debug("---------new_list_item NOT NULL---------------------------------------\r\n");
-    //   widget_use_style(new_list_item, "odd_clickable");
-    //   widget_layout_t layout;
-    //   widget_layout_parse(&layout, NULL, "t:5", NULL, "50");
-    //   widget_set_self_layout(new_list_item, &layout);
+          // <list_item style="odd_clickable" y="5" h="50" layout="r1 c0 s2 m2">
+          //   <view x="r:5" y="0" w="8%" h="50">
+          //     <image draw_type="icon" x="r:5" y="0" w="100%" h="50" image="a"/>
+          //   </view>
+          //   <view x="r:10" w="82%">
+          //     <label style="qq_main_black" x="r:5" y="m" w="100%" h="50" text="AWTK使用交流" />
+          //     <label style="qq_black" x="r:5" y="m:13" w="100%" h="20" text="您有一条新消息!" />
+          //   </view>
+          //   <view x="r:10" w="10%">
+          //     <label style="qq_black" x="r:10" y="m:-13" w="60" h="20" text="15:35" />
+          //     <image draw_type="icon" x="r:10" y="m:11" w="60" h="20" image="red_circle"/>
+          //     <label style="qq_num_index" x="r:10" y="m:11" w="60" h="20" text="3" />   
+          //   </view>
+          // </list_item>
+          // <list_item style="odd_clickable" y="5" h="50" layout="r1 c0 s2 m2">
+          //   <view x="r:5" y="0" w="8%" h="50">
+          //     <image draw_type="icon" x="r:5" y="0" w="100%" h="50" image="a"/>
+          //   </view>
+          //   <view x="r:10" w="82%">
+          //     <label style="qq_main_black" x="r:5" y="m" w="100%" h="50" text="致远QQ群" />
+          //     <label style="qq_black" x="r:5" y="m:13" w="100%" h="20" text="大家好啊!" />
+          //   </view>
+          //   <view x="r:10" w="10%">
+          //     <label style="qq_black" x="r:10" y="m:-13" w="60" h="20" text="15:35" />
+          //     <image draw_type="icon" x="r:10" y="m:11" w="60" h="20" image="red_circle"/>
+          //     <label style="qq_num_index" x="r:10" y="m:11" w="60" h="20" text="1" />   
+          //   </view>
+          // </list_item>
 
-    //   widget_set_children_layout_params(new_list_item, "r1 c0 s2 m2");
-    // }
+	  widget_t *new_list_item = list_item_create(qq_view, 0, 0, 32, 32);
+    
+    if (new_list_item != NULL) {
+      log_debug("---------new_list_item NOT NULL---------------------------------------\r\n");
+      widget_use_style(new_list_item, "odd_clickable");
+      widget_set_self_layout_params(new_list_item, "0", "5", "100%", "70");
+      widget_layout(new_list_item);
+      // widget_set_children_layout_params(new_list_item, "r1 c0 s2 m2");
+    
+      widget_t *new_view1 = view_create(new_list_item, 0, 0, 0, 0);
+      widget_set_self_layout_params(new_view1, "0", "0", "5%", "50");
+      widget_layout(new_view1);
 
+      widget_t *new_img1 = image_create(new_view1, 0, 0, 32, 32);
+      widget_set_self_layout_params(new_img1, "r:5", "0", "100%", "50");
+      widget_layout(new_img1);
+      image_set_draw_type(new_img1, IMAGE_DRAW_ICON);
+      image_set_image(new_img1, "a");
+
+      widget_t *new_view2 = view_create(new_list_item, 0, 0, 0, 0);
+      widget_set_self_layout_params(new_view2, "r:10", "0", "85%", "50");
+      widget_layout(new_view2);
+
+      widget_t *new_lab1 = label_create(new_view2, 0,0,0,0);
+      widget_use_style(new_lab1, "qq_main_black");
+      widget_set_self_layout_params(new_lab1, "0", "m:5", "100%", "50");
+      widget_layout(new_lab1);
+      widget_set_text_utf8(new_lab1, "AWTK使用交流");
+
+      widget_t *new_lab2 = label_create(new_view2, 0,0,0,0);
+      widget_use_style(new_lab2, "qq_black");
+      widget_set_self_layout_params(new_lab2, "0", "m:13", "100%", "20");
+      widget_layout(new_lab2);
+      widget_set_text_utf8(new_lab2, "您有一条新消息!");
+
+      widget_t *new_view3 = view_create(new_list_item, 0, 0, 0, 0);
+      widget_set_self_layout_params(new_view3, "r:10", "0", "10%", "50");
+      widget_layout(new_view3);
+
+      widget_t *new_lab3 = label_create(new_view3, 0,0,0,0);
+      widget_use_style(new_lab3, "qq_black");
+      widget_set_self_layout_params(new_lab3, "r:10", "m:-13", "60", "20");
+      widget_layout(new_lab3);
+      widget_set_text_utf8(new_lab3, "15:36");
+
+      widget_t *new_img2 = image_create(new_view3, 0, 0, 32, 32);
+      widget_set_self_layout_params(new_img2, "r:10", "m:11", "60", "20");
+      widget_layout(new_img2);
+      image_set_draw_type(new_img2, IMAGE_DRAW_ICON);
+      image_set_image(new_img2, "red_circle");
+
+      widget_t *new_lab4 = label_create(new_view3, 0,0,0,0);
+      widget_use_style(new_lab4, "qq_num_index");
+      widget_set_self_layout_params(new_lab4, "r:10", "m:11", "60", "20");
+      widget_layout(new_lab4);
+      widget_set_text_utf8(new_lab4, "1");
+    
+    }
     
   }
-
 
   return RET_OK;
 }
